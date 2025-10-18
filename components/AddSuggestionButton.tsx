4e737aa930725b77833d30afd15ae3f1cf2a40de
cejimenez57 <cejimenez5775@gmail.com>
2025-10-18 18:59:46 -0500

    changed button to unfaded

--- components/AddSuggestionButton.tsx
+++ components/AddSuggestionButton.tsx
@@ -8,8 +8,8 @@ export default function AddSuggestionButton({ onClick }: Props) {
       onClick={onClick}
       className="button mt-8 cursor-pointer hover:bg-[#1f3b5c]"
     >
-      <span className="opacity-70">Add Suggestion</span>
-      <img src="/turtle.png" alt="Turtle" className="w-6 h-6 opacity-70" />
+      <span>Add Suggestion</span>
+      <img src="/turtle.png" alt="Turtle" className="w-6 h-6" />
     </button>
   );
 }
